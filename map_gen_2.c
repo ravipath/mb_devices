e5da36797ad1529cd9f42ea9f956126fbe06bee0
Ravi Pathak <rav.tech.cs@gmail.com>
2023-11-09 10:05:34 +0100

    enhanced map generator

--- map_gen_2.c
+++ map_gen_2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include "mb_device_tcp.h"
 
 #define MAP_FILE_NAME "mb_data_map.c"
 #define START_HEADER "static mb_device_data_map_t mb_data_map[] = {"
@@ -21,16 +22,38 @@ int main(int argc, char *argv[])
 {
     FILE *csv_file = NULL;
     FILE *map_file = NULL;
+
     char buf[1024] = {0};
     char *bufPtr = buf;
     char single_line[200];
     const char *starttext = START_HEADER;
+
     int counter = 0;
     int curser = 0;
     int token_index = 0;
     int next_index[] = {2, 5, 6};
+
+    mbdevice_datatype_t dt;
+
     csv_file = fopen(argv[1], "r");
-    map_file = fopen(MAP_FILE_NAME, "wa");
+    if (csv_file == NULL)
+    {
+        fprintf(stderr, "CSV file missing\n");
+        fprintf(stderr, "Usage: ./map_generator [csv_file_path]\n");
+        return 0;
+    }
+    printf("CSV File: %s\n", argv[1]);
+    if(argv[2] == NULL){
+        fprintf(stderr, "No output file provided. An outfut file with default name will be generated: %s\n", MAP_FILE_NAME);
+        map_file = fopen(MAP_FILE_NAME, "wa");
+    }else{
+        map_file = fopen(argv[2], "wa");
+        if(map_file == NULL){
+            fprintf(stderr, "Failed to create output file %s\n", argv[2]);
+            return 0;
+        }
+    }
+    
 
     fprintf(map_file, "%s\n", starttext);
     fprintf(map_file, "%s\n", TAB);
@@ -39,6 +62,7 @@ int main(int argc, char *argv[])
     memset(buf, 0, 1024);
 
     char *tok;
+    printf("Read and process CSV file\n");
     while (fgets(buf, 1024, csv_file))
     {
         bufPtr = buf;
@@ -66,16 +90,19 @@ int main(int argc, char *argv[])
         {
             strcpy(single_line + curser, "MB_INT16");
             curser += strlen("MB_INT16");
+            dt = MB_INT16;
         }
         else if (strcmp(tok, "INT32") == 0)
         {
             strcpy(single_line + curser, "MB_INT32");
             curser += strlen("MB_INT32");
+            dt = MB_INT32;
         }
         if (strcmp(tok, "FLOAT32") == 0)
         {
             strcpy(single_line + curser, "MB_FLOAT32");
             curser += strlen("MB_FLOAT32");
+            dt = MB_FLOAT32;
         }
 
         strcpy(single_line + curser, bo);
@@ -88,26 +115,48 @@ int main(int argc, char *argv[])
         }
         strcpy(single_line + curser, tok);
         curser += strlen(tok);
+
         strcpy(single_line + curser, dp_value_range);
         curser += strlen(dp_value_range);
         strcpy(single_line + curser, "CONSTANT");
         curser += strlen("CONSTANT");
+
         strcpy(single_line + curser, max_value);
         curser += strlen(max_value);
         strcpy(single_line + curser, "-1");
         curser += strlen("-1");
+
         strcpy(single_line + curser, min_value);
         curser += strlen(min_value);
         strcpy(single_line + curser, "-1");
         curser += strlen("-1");
-        strcpy(single_line + curser, i_data);
-        curser += strlen(i_data);
-        strcpy(single_line + curser, "23");
-        curser += strlen("23");
+        switch (dt)
+        {
+        case MB_INT8:
+        case MB_INT16:
+        case MB_INT32:
+            strcpy(single_line + curser, i_data);
+            curser += strlen(i_data);
+            strcpy(single_line + curser, "100");
+            curser += strlen("23");
+            break;
+        case MB_FLOAT32:
+            strcpy(single_line + curser, f_data);
+            curser += strlen(f_data);
+            strcpy(single_line + curser, "1234.56");
+            curser += strlen("1234.56");
+            break;
+        case MB_UNKNOWN:
+            break;
+        default:
+            break;
+        }
+
         strcpy(single_line + curser, step);
         curser += strlen(step);
         strcpy(single_line + curser, "-1");
         curser += strlen("-1");
+
         strcpy(single_line + curser, endl_delimiter);
         curser += strlen(endl_delimiter);
         fprintf(map_file, "%s", single_line);
@@ -115,6 +164,8 @@ int main(int argc, char *argv[])
         counter = 0;
         curser = 0;
     }
+    printf("Processing done!\n");
+    printf("Output written to the file\n");
     fprintf(map_file, "%s\n", END_HEADER);
     fclose(csv_file);
     return 0;
--- mb_data_map.c
+++ mb_data_map.c
@@ -1,17 +1,8 @@
 static mb_device_data_map_t mb_data_map[] = {
     
-{.start_addr = 0, .datatype = MB_INT32, .bo = BADC, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 2, .datatype = MB_INT32, .bo = BADC, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 4, .datatype = MB_INT32, .bo = BADC, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 6, .datatype = MB_INT32, .bo = BADC, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 8, .datatype = MB_INT32, .bo = BADC, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 10, .datatype = MB_INT32, .bo = BADC, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 12, .datatype = MB_INT16, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 13, .datatype = MB_INT16, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 14, .datatype = MB_INT16, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 15, .datatype = MB_INT16, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 16, .datatype = MB_INT16, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 17, .datatype = MB_INT16, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 18, .datatype = MB_INT16, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
-{.start_addr = 19, .datatype = MB_INT16, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 23, .step = -1},
+{.start_addr = 0, .datatype = MB_INT16, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 10, .step = -1},
+{.start_addr = 1, .datatype = MB_INT32, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 10, .step = -1},
+{.start_addr = 3, .datatype = MB_INT32, .bo = BADC, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 10, .step = -1},
+{.start_addr = 5, .datatype = MB_FLOAT32, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.f_data = 1234.56, .step = -1},
+{.start_addr = 7, .datatype = MB_FLOAT32, .bo = DCBA, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.f_data = 1234.56, .step = -1},
 };
