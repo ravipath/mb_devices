7a99e0009070a1956a412393b9ddf97d7e4f914e
Ravi Pathak <rav.tech.cs@gmail.com>
2024-01-16 11:12:39 +0100

    works

--- mb_data_map.c
+++
@@ -1,8 +0,0 @@
-static mb_device_data_map_t mb_data_map[] = {
-    
-{.start_addr = 0, .datatype = MB_INT16, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 10, .step = -1},
-{.start_addr = 1, .datatype = MB_INT32, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 10, .step = -1},
-{.start_addr = 3, .datatype = MB_INT32, .bo = BADC, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.i_data = 10, .step = -1},
-{.start_addr = 5, .datatype = MB_FLOAT32, .bo = ABCD, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.f_data = 1234.56, .step = -1},
-{.start_addr = 7, .datatype = MB_FLOAT32, .bo = DCBA, .dp_value_range = CONSTANT, .max_value = -1, .min_value = -1, .value.f_data = 1234.56, .step = -1},
-};
